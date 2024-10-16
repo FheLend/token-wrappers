a3b86cfe8d1135b4a98a0a4d0f7754578e396393
halink0803 <halink0803@gmail.com>
2024-10-16 17:36:12 +0700

    using only encrypted amount type

--- contracts/testToken.sol
+++ contracts/testToken.sol
@@ -7,21 +7,32 @@ import "@fhenixprotocol/contracts/FHE.sol";
 
 contract TestToken is ERC20, Permissioned {
 
-    mapping(address => euint128) internal _encBalances;
+    mapping(address => euint64) internal _encBalances;
     mapping(address => bool) internal _minted; // limit each address to mint only one
+    euint64 public totalEncryptedSupply;
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 100000 * 10 ** uint(decimals()));
+    }
+
+    // override token decimals
+    function decimals() public override view virtual returns (uint8) {
+        return 6;
     }
 
     // @dev mint token for testing purpose
-    function mint() public {
+    function mint(inEuint64 memory eMintAmount) public {
         // check require
         require(_minted[msg.sender] == false, "Each address can mint only once.");
-        // mint 100 token to msg sender 
-        _mint(msg.sender, 100 * 10 ** uint(decimals()));
+        _mintEncrypted(msg.sender, eMintAmount);
         _minted[msg.sender] = true;
     }
+
+    function _mintEncrypted(address to, inEuint64 memory encryptedAmount) internal {
+        euint64 amount = FHE.asEuint64(encryptedAmount);
+        _encBalances[to] = _encBalances[to] + amount;
+        totalEncryptedSupply = totalEncryptedSupply + amount;
+    }
+
 
     function wrap(uint32 amount) public {
         // Make sure that the sender has enough of the public balance
@@ -30,13 +41,13 @@ contract TestToken is ERC20, Permissioned {
         _burn(msg.sender, amount);
 
         // convert public amount to shielded by encrypting it
-        euint128 shieldedAmount = FHE.asEuint128(amount);
+        euint64 shieldedAmount = FHE.asEuint64(amount);
         // Add shielded balance to his current balance
         _encBalances[msg.sender] = _encBalances[msg.sender] + shieldedAmount;
     }
 
-    function unwrap(inEuint128 memory amount) public {
-        euint128 _amount = FHE.asEuint128(amount);
+    function unwrap(inEuint64 memory amount) public {
+        euint64 _amount = FHE.asEuint64 (amount);
         // verify that our shielded balance is greater or equal than the requested amount 
         FHE.req(_encBalances[msg.sender].gte(_amount));
         // subtract amount from shielded balance
@@ -45,23 +56,15 @@ contract TestToken is ERC20, Permissioned {
         _mint(msg.sender, FHE.decrypt(_amount));
     }
 
-    function transferEncrypted(address to, inEuint128 calldata encryptedAmount) public {
-        euint128 amount = FHE.asEuint128(encryptedAmount);
+    function transferEncrypted(address to, inEuint64 calldata encryptedAmount) public {
+        euint64 amount = FHE.asEuint64(encryptedAmount);
         // Make sure the sender has enough tokens.
         FHE.req(amount.lte(_encBalances[msg.sender]));
 
         // Add to the balance of `to` and subract from the balance of `from`.
         _encBalances[to] = _encBalances[to] + amount;
         _encBalances[msg.sender] = _encBalances[msg.sender] - amount;
-    }
-    
-    function getBalanceEncrypted(Permission calldata perm) 
-    public 
-    view 
-    onlySender(perm) 
-    returns (uint256) {
-        return FHE.decrypt(_encBalances[msg.sender]);
-    }
+    } 
 
     function getBalanceSealed(
         Permission memory permission
